97838f2979494a3c423ef23aa1294ffd46a11adc
BlackEagle <eagle.thomas@gmail.com>
2012-11-22 23:04:38 +0100

    Init i18n

---
+++ app/core/i18n/I18nHandler.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of I18nHandler
+ *
+ * @author Thom
+ */
+class I18nHandler {
+    //put your code here
+}
+
+?>
